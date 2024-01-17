1ffdfd9ad80c2ede9890c33114057bb5841bf3e5
eternal17 <sarmadkhatri15@gmail.com>
2024-01-17 19:02:47 +0000

    Refactor home page layout

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,53 +1,16 @@
-import { Box, Button, TextField, Typography } from '@mui/material';
-import { useState, MouseEvent, ChangeEvent } from 'react';
+import { Grid } from '@mui/material';
 import SavedJob from './components/SavedJob';
 
 const Homepage = () => {
-  const [inputText, setInputText] = useState('');
-
-  const handleSumbit = (e: MouseEvent<HTMLButtonElement>) => {
-    // send the prompt along with the job description to the OpenAi API
-    e.preventDefault();
-    console.log(inputText);
-  };
-
   return (
     <>
-      <Box
-        component="div"
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        flexDirection="column"
-        height="100vh"
-      >
-        <Typography variant="h6">Copy Job Description</Typography>
-        <Box
-          component="div"
-          display="flex"
-          gap="10px"
-          justifyContent="center"
-          alignItems="center"
-        >
-          <TextField
-            type="text"
-            value={inputText}
-            size="medium"
-            multiline
-            onChange={(e: ChangeEvent<HTMLInputElement>) => {
-              setInputText(e.target.value);
-            }}
-          />
-          <Button
-            onClick={handleSumbit}
-            variant="contained"
-            sx={{ height: '24px' }}
-          >
-            Upload
-          </Button>
-        </Box>
+      <Grid border={'1px solid green'} container gap={2} padding={4}>
         <SavedJob />
-      </Box>
+        <SavedJob />
+        <SavedJob />
+        <SavedJob />
+        <SavedJob />
+      </Grid>
     </>
   );
 };
