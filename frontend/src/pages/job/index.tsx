39d8dfd7cc17652ebf8a0f60cc086c028942629d
rickyadriell <53133228+codecplyre@users.noreply-github.com>
2024-01-18 13:38:24 +0000

    Add job title and job description from jobs.json

--- frontend/src/pages/job/index.tsx
+++ frontend/src/pages/job/index.tsx
@@ -1,6 +1,8 @@
 import { Button, Grid, Typography } from '@mui/material';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+// import jobs.json from repository/jobs.json and use it to populate the job title and job description
+import jobs from '../../repository/jobs.json';
 
 const Job = () => {
     const { id } = useParams();
@@ -8,17 +10,45 @@ const Job = () => {
     const [jobDescription, setJobDescription] = useState(
         'Empty Job Description'
     );
+    useEffect(() => {
+        // set job title and job description
+        const job = jobs.find((job) => job.id === id);
+        if (job) {
+            setJobTitle(job.title);
+            setJobDescription(job.description);
+        }
+    }, [id]);
     return (
-        <Grid container item direction={'column'}>
-            <Grid item xs={6} md={12}>
-                <Typography variant='h5'>Job Title</Typography>
-                <Typography variant='h6'>{jobTitle}</Typography>
-                <Typography variant='h5'>Job Description</Typography>
-                <Typography variant='body1'>{jobDescription}</Typography>
+        <Grid container item direction={'row'}>
+            <Grid item xs={12} md={6}>
+                <Typography variant='body1'>Job Title:</Typography>
+                <Typography variant='h5'>{jobTitle}</Typography>
+                <Typography variant='body1'>Job Description</Typography>
+                {/* <Typography variant='body1'>{jobDescription}</Typography> */}
+                {jobDescription.split('\n').map((line, index) => {
+                    if (line.length < 50) {
+                        return (
+                            <Typography
+                                variant='h6'
+                                fontWeight='bold'
+                                key={index}
+                                align='justify'>
+                                {line}
+                            </Typography>
+                        );
+                    }
+                    return (
+                        <Typography variant='h6' key={index} align='justify'>
+                            {line}
+                        </Typography>
+                    );
+                })}
             </Grid>
-            <Grid item xs={6} md={12} direction={'column'}>
+            <Grid item xs={12} md={6} direction={'column'}>
                 <Grid container item>
-                    <Button variant='contained'>Generate</Button>
+                    <Button variant='contained' size='small'>
+                        Generate
+                    </Button>
                 </Grid>
             </Grid>
         </Grid>
