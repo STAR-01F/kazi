f45be5312f0219beb61e7d30f01a1f1c6e6b1e51
rickyadriell <53133228+codecplyre@users.noreply-github.com>
2024-01-19 11:56:06 +0000

    Refactor Keywords component to handle loading state and display skeleton chips

--- frontend/src/pages/job/components/Keywords.tsx
+++ frontend/src/pages/job/components/Keywords.tsx
@@ -1,5 +1,36 @@
-const Keywords = () => {
-    return <div>Keywords</div>;
+import { Chip, Typography } from '@mui/material';
+import { SkeletonChip } from '@components/skeleton';
+
+type KeywordsProps = {
+    keywords: string[];
+    isLoading: boolean;
+};
+
+const Keywords = ({ keywords, isLoading }: KeywordsProps) => {
+    if (!isLoading) {
+        return (
+            <>
+                {[30, 30, 30, 40, 40, 30, 30, 30, 50].map((size, index) => (
+                    <SkeletonChip key={index} size={size} />
+                ))}
+            </>
+        );
+    }
+    return (
+        <>
+            {keywords &&
+                keywords.map((keyword, index) => {
+                    return (
+                        <Chip
+                            key={index}
+                            label={
+                                <Typography variant='h6'>{keyword}</Typography>
+                            }
+                        />
+                    );
+                })}
+        </>
+    );
 };
 
 export default Keywords;
