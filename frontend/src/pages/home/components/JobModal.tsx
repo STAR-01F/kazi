10cd29da040d8091544bf204f2488fa21cef69d7
rickyadriell <53133228+rickyadriell@users.noreply.github.com>
2024-01-23 16:04:24 +0000

    Add job creation functionality to JobModal component

--- frontend/src/pages/home/components/JobModal.tsx
+++ frontend/src/pages/home/components/JobModal.tsx
@@ -7,63 +7,96 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import AddJob from './AddJob';
+import CreateJob from '@utils/savejob';
+import type { Job } from 'src/@types';
 
 export default function JobModal() {
-  const [open, setOpen] = React.useState(false);
+    const [open, setOpen] = React.useState(false);
+    const [job, setJob] = React.useState<Partial<Job>>();
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+    const handleClose = () => {
+        setOpen(false);
+    };
 
-  return (
-    <React.Fragment>
-      <AddJob handleClickOpen={handleClickOpen}/>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        PaperProps={{
-          component: 'form',
-          onSubmit: (event: React.FormEvent<HTMLFormElement>) => {
-            event.preventDefault();
-            const formData = new FormData(event.currentTarget);
-            const formJson = Object.fromEntries((formData).entries());
-            console.log(formJson)
-            handleClose();
-          },
-        }}
-      >
-        <DialogTitle>Add Job</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Please enter the details for the new job.
-          </DialogContentText>
-          <TextField
-            autoFocus
-            required
-            sx={{marginBottom: 2}}
-            id="name"
-            name="job"
-            label="Job Title"
-            fullWidth
-             variant="standard"
-                  />
-          <TextField
-            id="outlined-textarea"
-            label="Job Description"
-            placeholder=""
-            multiline
-            fullWidth
-            />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button type="submit">Save</Button>
-        </DialogActions>
-      </Dialog>
-    </React.Fragment>
-  );
+    const handleAddJob = () => {
+        if (!job) return;
+        CreateJob(job);
+    };
+
+    return (
+        <React.Fragment>
+            <AddJob handleClickOpen={handleClickOpen} />
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                PaperProps={{
+                    component: 'form',
+                    onSubmit: (event: React.FormEvent<HTMLFormElement>) => {
+                        event.preventDefault();
+                        const formData = new FormData(event.currentTarget);
+                        const formJson = Object.fromEntries(formData.entries());
+                        console.log(formJson);
+                        handleClose();
+                    },
+                }}>
+                <DialogTitle>Add Job</DialogTitle>
+                <DialogContent>
+                    <DialogContentText mb={1}>
+                        Please enter the details for the new job.
+                    </DialogContentText>
+                    <TextField
+                        autoFocus
+                        required
+                        sx={{ marginBottom: 2 }}
+                        id='company-name'
+                        name='company'
+                        label='Company Name'
+                        placeholder=''
+                        value={job?.Company}
+                        onChange={(e) =>
+                            setJob(() => ({ Company: e.target.value }))
+                        }
+                        fullWidth
+                    />
+                    <TextField
+                        autoFocus
+                        required
+                        sx={{ marginBottom: 2 }}
+                        id='job-title'
+                        name='job'
+                        label='Job Title'
+                        placeholder=''
+                        value={job?.Title}
+                        onChange={(e) =>
+                            setJob((j) => ({ ...j, Title: e.target.value }))
+                        }
+                        fullWidth
+                    />
+                    <TextField
+                        id='job-description'
+                        label='Job Description'
+                        name='description'
+                        placeholder=''
+                        value={job?.Description}
+                        onChange={(e) =>
+                            setJob({ ...job, Description: e.target.value })
+                        }
+                        multiline
+                        fullWidth
+                        required
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button type='submit' onClick={handleAddJob}>
+                        Save
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </React.Fragment>
+    );
 }
