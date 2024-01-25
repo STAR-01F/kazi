d3063643fe51364555ed513004d53242f74733b1
eternal17 <sarmadkhatri15@gmail.com>
2024-01-25 19:43:58 +0000

    fix: [TRAC-35] Add Keywords field to Job type

--- frontend/src/@types/job.ts
+++ frontend/src/@types/job.ts
@@ -1,11 +1,12 @@
 type Job = {
-    ID?: string;
-    Title: string;
-    Description: string;
-    Company: string;
-    CreatedAt?: string;
-    UpdatedAt?: string;
-    Status?: string;
+  ID?: string;
+  Title: string;
+  Description: string;
+  Company: string;
+  CreatedAt?: string;
+  UpdatedAt?: string;
+  Status?: string;
+  Keywords?: string;
 };
 
 export default Job;
