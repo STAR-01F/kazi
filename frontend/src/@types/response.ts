475c1d4068d258d2f8a262ed60ee10b58bde4897
eternal17 <sarmadkhatri15@gmail.com>
2024-02-05 12:38:51 +0000

    refactor: [TRAC-49] updated status in each Response type

--- frontend/src/@types/response.ts
+++ frontend/src/@types/response.ts
@@ -1,13 +1,13 @@
 type Response<T, U> = Success<T> | Error<U>;
 
 export type Success<T> = {
-    status: string;
-    message: string;
-    data: T;
+  status: 'Success';
+  message: string;
+  data: T;
 };
 export type Error<U> = {
-    status: string;
-    message: U;
+  status: 'Error';
+  message: U;
 };
 
 export default Response;
