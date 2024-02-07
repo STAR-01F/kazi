601ebbf9c677b1fa9c820a06fec7cfb19f2cb2f2
rickyadriell <53133228+rickyadriell@users.noreply.github.com>
2024-02-07 21:35:23 +0000

    feat: [TRAC-78] Add UpdateJob and DeleteJob functions to job service

--- frontend/src/services/firebase/jobs/index.ts
+++ frontend/src/services/firebase/jobs/index.ts
@@ -1,4 +1,6 @@
 import {GetJobByJobID, GetJobsByUserID} from './GetJobs';
 import {CreateJob} from './CreateJob';
+import {DeleteJob} from './DeleteJob';
+import {UpdateJob} from './UpdateJob';
 
-export {GetJobByJobID, GetJobsByUserID, CreateJob};
+export {GetJobByJobID, GetJobsByUserID, CreateJob, DeleteJob, UpdateJob};
