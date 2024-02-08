d7280055a97a9a61cb105522ae3fbfad4634e1f5
rickyadriell <53133228+rickyadriell@users.noreply.github.com>
2024-02-08 13:32:58 +0000

    feat: [TRAC-79] Update JobModal component
     separate the link and manual modal

--- frontend/src/pages/home/components/JobModal.tsx
+++ frontend/src/pages/home/components/JobModal.tsx
@@ -9,7 +9,7 @@ import DialogTitle from '@mui/material/DialogTitle';
 import AddJob from './AddJob';
 import CreateJob from '@utils/savejob';
 import type {Job} from 'src/@types';
-import {FormControl, InputLabel, MenuItem, Select} from '@mui/material';
+import {FormControl, InputLabel, Link, MenuItem, Select} from '@mui/material';
 import {useNavigate} from 'react-router-dom';
 
 const JobStatus = [
@@ -20,20 +20,16 @@ const JobStatus = [
   'Rejected',
   'Accepted',
 ];
-export default function JobModal() {
-  const [open, setOpen] = React.useState(false);
+type ManualProps = {
+  toggle: () => void;
+  onClose: () => void;
+};
+const Manual = ({toggle, onClose}: ManualProps) => {
   const [company, setCompany] = React.useState('');
   const [title, setTitle] = React.useState('');
   const [description, setDescription] = React.useState('');
   const [status, setStatus] = React.useState('');
   const navigate = useNavigate();
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
   const handleAddJob = async () => {
     const job: Partial<Job> = {
       company: company,
@@ -46,97 +42,151 @@ export default function JobModal() {
     const resp = await CreateJob(job);
     console.log(resp);
     // check if resp is an error
-    if (resp.status === 'error') {
+    if (resp.status === 'Error') {
       return;
     }
 
     setTitle('');
     setDescription('');
-    handleClose();
+    onClose();
     // data returned is the jobId is navigated to.
     if ('data' in resp) {
       navigate(`/job/${resp.data}`);
     }
   };
+  return (
+    <>
+      <DialogContent>
+        <DialogContentText mb={1}>
+          Please enter the details for the new job.
+        </DialogContentText>
+        <TextField
+          autoFocus
+          required
+          sx={{marginBottom: 2}}
+          id="job-title"
+          name="job"
+          label="Job Title"
+          placeholder=""
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          fullWidth
+        />
+        <TextField
+          autoFocus
+          required
+          sx={{marginBottom: 2}}
+          id="company-name"
+          name="company"
+          label="Company"
+          value={company}
+          onChange={(e) => setCompany(e.target.value)}
+          fullWidth
+        />
+        <FormControl fullWidth sx={{mb: 2}}>
+          <InputLabel id="job-status-input">Job Status</InputLabel>
+          <Select
+            labelId="job-status-input"
+            id="job-status-select"
+            label="Job Status"
+            value={status}
+            onChange={(e) => setStatus(e.target.value)}
+          >
+            {JobStatus.map((status, i) => (
+              <MenuItem key={`${status}-${i}`} value={status}>
+                {status}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <TextField
+          sx={{mb: 2}}
+          id="job-description"
+          label="Job Description"
+          name="description"
+          placeholder=""
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          multiline
+          fullWidth
+          required
+        />
+        <DialogContentText mb={1}>
+          or add <Link onClick={toggle}>link</Link>
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose}>Cancel</Button>
+        <Button type="submit" onClick={handleAddJob}>
+          Save
+        </Button>
+      </DialogActions>
+    </>
+  );
+};
+type JobLinkModalProps = {
+  toggle: () => void;
+  onClose: () => void;
+};
+const JobLinkModal = ({toggle, onClose}: JobLinkModalProps) => {
+  const [jobLink, setJobLink] = React.useState('');
+  const handleAddJob = () => {};
+  return (
+    <>
+      <DialogContent>
+        <DialogContentText mb={1}>
+          Please enter the link of the job.
+        </DialogContentText>
+        <TextField
+          autoFocus
+          required
+          sx={{marginBottom: 2}}
+          id="job-link"
+          name="job-link"
+          label="Job Link"
+          placeholder=""
+          value={jobLink}
+          onChange={(e) => setJobLink(e.target.value)}
+          fullWidth
+        />
+        <DialogContentText mb={1}>
+          or add <Link onClick={toggle}>manually</Link>
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose}>Cancel</Button>
+        <Button type="submit" onClick={handleAddJob}>
+          Save
+        </Button>
+      </DialogActions>
+    </>
+  );
+};
+export default function JobModal() {
+  const [open, setOpen] = React.useState(false);
+  const [openManual, setManualOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const toggle = () => {
+    setManualOpen((prev) => !prev);
+  };
 
   return (
     <React.Fragment>
       <AddJob handleClickOpen={handleClickOpen} />
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        PaperProps={{
-          component: 'form',
-          onSubmit: (event: React.FormEvent<HTMLFormElement>) => {
-            event.preventDefault();
-            const formData = new FormData(event.currentTarget);
-            const formJson = Object.fromEntries(formData.entries());
-            console.log(formJson);
-            handleClose();
-          },
-        }}
-      >
+      <Dialog open={open} onClose={handleClose} fullWidth maxWidth="xs">
         <DialogTitle>Add Job</DialogTitle>
-        <DialogContent>
-          <DialogContentText mb={1}>
-            Please enter the details for the new job.
-          </DialogContentText>
-          <TextField
-            autoFocus
-            required
-            sx={{marginBottom: 2}}
-            id="job-title"
-            name="job"
-            label="Job Title"
-            placeholder=""
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            fullWidth
-          />
-          <TextField
-            autoFocus
-            required
-            sx={{marginBottom: 2}}
-            id="company-name"
-            name="company"
-            label="Company"
-            onChange={(e) => setCompany(e.target.value)}
-            fullWidth
-          />
-          <FormControl fullWidth sx={{mb: 2}}>
-            <InputLabel id="job-status-input">Job Status</InputLabel>
-            <Select
-              labelId="job-status-input"
-              id="job-status-select"
-              label="Job Status"
-              value={status}
-              onChange={(e) => setStatus(e.target.value)}
-            >
-              {JobStatus.map((status, i) => (
-                <MenuItem key={`${status}-${i}`} value={status}>
-                  {status}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <TextField
-            id="job-description"
-            label="Job Description"
-            name="description"
-            placeholder=""
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            multiline
-            fullWidth
-            required
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button type="submit" onClick={handleAddJob}>
-            Save
-          </Button>
-        </DialogActions>
+        {openManual ? (
+          <Manual toggle={toggle} onClose={handleClose} />
+        ) : (
+          <JobLinkModal toggle={toggle} onClose={handleClose} />
+        )}
       </Dialog>
     </React.Fragment>
   );
