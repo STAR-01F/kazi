d73d2f1b829c7fd0328949d264b63cb2e4fde050
eternal17 <sarmadkhatri15@gmail.com>
2024-02-09 10:49:34 +0000

    refactor: [TRAC-82] updated main file to only call the lambda function

--- api/scraper/main.go
+++ api/scraper/main.go
@@ -1,32 +1,9 @@
 package main
 
 import (
-	"log/slog"
-	"net/http"
-	"os"
-	"time"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-
-	mux := http.NewServeMux()
-
-	s := &http.Server{
-		Addr:           ":" + port,
-		Handler:        mux,
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
-		MaxHeaderBytes: 1 << 20,
-	}
-
-	mux.HandleFunc("/scrape", handleScrape)
-
-	slog.Info("Starting server on port %s", "port", port)
-	if err := s.ListenAndServe(); err != nil {
-		slog.Error("Failed to start server: %v", err)
-	}
+	lambda.Start(handleOttaScrape)
 }
