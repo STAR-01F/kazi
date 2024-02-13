7db62e9b62ba4c375574c739267395e404c30f34
rickyadriell <53133228+rickyadriell@users.noreply.github.com>
2024-02-13 21:51:14 +0000

    style: [TRAC-96] Refactor SavedJob component and homepage layout

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,4 +1,4 @@
-import {Grid} from '@mui/material';
+import {Box, Grid} from '@mui/material';
 import SavedJob from './components/SavedJob';
 import JobModal from './components/JobModal';
 import useFetchJobs from '@hooks/useFetchJobs';
@@ -6,20 +6,38 @@ import useFetchJobs from '@hooks/useFetchJobs';
 const Homepage = () => {
   const jobs = useFetchJobs();
   return (
-    <Grid container gap={2} padding={4}>
-      <JobModal />
-      {jobs.data &&
-        jobs.data.map((job) => {
-          return (
-            <SavedJob
-              key={job.id}
-              companyName={job.company}
-              jobTitle={job.title}
-              jobID={job.id!}
-              logoPath="../src/assets/google-logo.png"
-            />
-          );
-        })}
+    <Grid
+      id="home-page"
+      container
+      item
+      maxWidth="lg"
+      direction={'column'}
+      wrap="nowrap"
+      padding={{xs: '10px 20px', md: '15px 30px', lg: '20px 40px'}}
+    >
+      <Box id="home-page-header" mb={3} display={'flex'}>
+        <JobModal />
+      </Box>
+      <Grid
+        id="home-page-jobs-container"
+        container
+        item
+        gap={2}
+        // maxHeight={'max-content'}
+      >
+        {jobs.data &&
+          jobs.data.map((job) => {
+            return (
+              <SavedJob
+                key={job.id}
+                companyName={job.company}
+                jobTitle={job.title}
+                jobID={job.id!}
+                logoPath="../src/assets/google-logo.png"
+              />
+            );
+          })}
+      </Grid>
     </Grid>
   );
 };
