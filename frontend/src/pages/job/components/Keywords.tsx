e2e22128dba6b8bfe6c409bb1624fe018eda3eca
eternal17 <sarmadkhatri15@gmail.com>
2024-02-14 22:51:42 +0000

    refactor: [TRAC-29] added some padding to the chip

--- frontend/src/pages/job/components/Keywords.tsx
+++ frontend/src/pages/job/components/Keywords.tsx
@@ -1,36 +1,35 @@
-import { Chip, Typography } from '@mui/material';
-import { SkeletonChip } from '@components/skeleton';
+import {Chip, Typography} from '@mui/material';
+import {SkeletonChip} from '@components/skeleton';
 
 type KeywordsProps = {
-    keywords: string[];
-    isLoading: boolean;
+  keywords: string[];
+  isLoading: boolean;
 };
 
-const Keywords = ({ keywords, isLoading }: KeywordsProps) => {
-    if (isLoading) {
-        return (
-            <>
-                {[30, 30, 30, 40, 40, 30, 30, 30, 50].map((size, index) => (
-                    <SkeletonChip key={index} size={size} />
-                ))}
-            </>
-        );
-    }
+const Keywords = ({keywords, isLoading}: KeywordsProps) => {
+  if (isLoading) {
     return (
-        <>
-            {keywords &&
-                keywords.map((keyword, index) => {
-                    return (
-                        <Chip
-                            key={index}
-                            label={
-                                <Typography variant='h6'>{keyword}</Typography>
-                            }
-                        />
-                    );
-                })}
-        </>
+      <>
+        {[30, 30, 30, 40, 40, 30, 30, 30, 50].map((size, index) => (
+          <SkeletonChip key={index} size={size} />
+        ))}
+      </>
     );
+  }
+  return (
+    <>
+      {keywords &&
+        keywords.map((keyword, index) => {
+          return (
+            <Chip
+              key={index}
+              label={<Typography variant="h6">{keyword}</Typography>}
+              sx={{padding: '1rem'}}
+            />
+          );
+        })}
+    </>
+  );
 };
 
 export default Keywords;
