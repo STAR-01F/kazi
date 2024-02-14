2e4d441a045c45b88215cdf735c60380021bd359
eternal17 <sarmadkhatri15@gmail.com>
2024-02-14 15:57:45 +0000

    refactor: [TRAC-29] changing description text to body text

--- frontend/src/pages/job/components/Description.tsx
+++ frontend/src/pages/job/components/Description.tsx
@@ -1,31 +1,32 @@
-import { Typography } from '@mui/material';
+import {Typography} from '@mui/material';
 
 type DescriptionProps = {
-    description: string;
+  description: string;
 };
-const Description = ({ description }: DescriptionProps) => {
-    return (
-        <>
-            {description.split('\n').map((line, index) => {
-                if (line.length < 50) {
-                    return (
-                        <Typography
-                            variant='h6'
-                            fontWeight='bold'
-                            key={index}
-                            align='justify'>
-                            {line}
-                        </Typography>
-                    );
-                }
-                return (
-                    <Typography variant='h6' key={index} align='justify'>
-                        {line}
-                    </Typography>
-                );
-            })}
-        </>
-    );
+const Description = ({description}: DescriptionProps) => {
+  return (
+    <>
+      {description.split('\n').map((line, index) => {
+        if (line.length < 50) {
+          return (
+            <Typography
+              variant="h6"
+              fontWeight="bold"
+              key={index}
+              align="justify"
+            >
+              {line}
+            </Typography>
+          );
+        }
+        return (
+          <Typography variant="body1" key={index} align="justify">
+            {line}
+          </Typography>
+        );
+      })}
+    </>
+  );
 };
 
 export default Description;
