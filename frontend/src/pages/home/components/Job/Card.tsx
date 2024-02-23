ad9f5a0e81d88eb254eaa34c7caa1d0e3183826f
rickyadriell <53133228+rickyadriell@users.noreply.github.com>
2024-02-23 23:38:22 +0000

    feat: [TRAC-117] Refactor JobCard component to include company logo and adjust card layout

--- frontend/src/pages/home/components/Job/Card.tsx
+++ frontend/src/pages/home/components/Job/Card.tsx
@@ -4,20 +4,16 @@ import {
   Typography,
   Stack,
   Button,
-  Avatar,
   Grid,
   CardActions,
   Paper,
+  CardMedia,
+  Box,
 } from '@mui/material';
 import {Link} from 'react-router-dom';
 import MenuListButton from '@components/button/MenuListButton';
 import {useAuth} from '@services/firebase/hooks/useAuth';
 import {DeleteJob, UpdateJobStatus} from '@services/firebase/jobs';
-
-// props to be passed in should be job title and company, possibly logo. Using ts for the props.
-
-// TODO
-// add company logo
 
 type JobCardProps = {
   companyName: string;
@@ -63,37 +59,58 @@ const JobCard = ({companyName, jobTitle, logoPath, jobID}: JobCardProps) => {
       component={Paper}
       variant="outlined"
       sx={{
-        minWidth: '350px',
+        // minWidth: '350px',
         maxWidth: {
           xs: '100%',
-          md: 'calc(50% - 8.5px)',
-          lg: 'calc(33.1% - 8.5px)',
+          sm: 'calc((100% - (1 * 16px))/2)',
+          md: 'calc((100% - (2 * 16px))/3)',
+          lg: 'calc((100% - (3 * 16px))/4)',
         },
         width: '100%',
         height: 'max-content',
       }}
     >
+      {logoPath ? (
+        <CardMedia
+          component="img"
+          alt={companyName}
+          sx={{
+            height: '100px',
+            objectFit: 'contain',
+            p: '10px',
+          }}
+          image={'https://images.otta.com/search/width_400/' + logoPath}
+        />
+      ) : (
+        <Box
+          sx={{
+            height: '100px',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            p: '10px',
+          }}
+        >
+          <Typography variant="h5">{companyName}</Typography>
+        </Box>
+      )}
       <CardContent>
         <Grid container justifyContent={'space-between'}>
-          <Stack>
-            <Typography fontSize={20} fontWeight={'bold'}>
+          <Stack sx={{width: '100%'}}>
+            <Typography fontSize={20} fontWeight={'bold'} noWrap>
               {companyName}
             </Typography>
-            <Typography>{jobTitle}</Typography>
+            <Typography noWrap>{jobTitle}</Typography>
           </Stack>
-          <Avatar
-            alt={companyName}
-            src={logoPath}
-            sx={{height: 'auto', width: '55px'}}
-          />
         </Grid>
       </CardContent>
       <CardActions>
         <Button
-          variant={'contained'}
+          variant={'outlined'}
           component={Link}
           to={`job/${jobID}`}
           size="small"
+          fullWidth
         >
           View
         </Button>
@@ -101,8 +118,9 @@ const JobCard = ({companyName, jobTitle, logoPath, jobID}: JobCardProps) => {
           variant="contained"
           size="small"
           menuActionList={moveMenulist}
+          fullWidth
         >
-          Move
+          Actions
         </MenuListButton>
       </CardActions>
     </Card>
