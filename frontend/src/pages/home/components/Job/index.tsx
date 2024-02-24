603d8a5be68762af6c4d63bdca621f33dc286056
rickyadriell <53133228+rickyadriell@users.noreply.github.com>
2024-02-24 20:44:20 +0000

    refactor: [TRAC-115] Refactor JobSection component and add support for different views

--- frontend/src/pages/home/components/Job/index.tsx
+++ frontend/src/pages/home/components/Job/index.tsx
@@ -3,31 +3,47 @@ import {Job} from 'src/@types';
 import GridView from './GridView';
 import LoadingGridView from './LoadingGridView';
 import Empty from './Empty';
+import {useSearchParams} from 'react-router-dom';
 
 type JobStatus = 'Saved' | 'Applied' | 'Interview ' | 'Rejected';
 
 type JobByStatus = {
   [status in JobStatus]: Job[];
 };
-function JobSection() {
+
+const groupJobsByStatus = (jobs: Job[]): JobByStatus => {
+  return jobs.reduce((acc, job) => {
+    const status = job.status as JobStatus;
+    if (!acc[status]) {
+      acc[status] = [];
+    }
+    acc[status].push(job);
+    return acc;
+  }, {} as JobByStatus);
+};
+
+const JobSection = () => {
   const jobs = useFetchJobs();
+  const [searchParam] = useSearchParams();
+  const view = searchParam.get('view') || 'grid';
   const jobByStatus = jobs.data
-    ? jobs.data.reduce((acc, job) => {
-        const status = job.status as JobStatus;
-        if (!acc[status]) {
-          acc[status] = [];
-        }
-        acc[status].push(job);
-        return acc;
-      }, {} as JobByStatus)
+    ? groupJobsByStatus(jobs.data)
     : ({} as JobByStatus);
-  return jobs.status === 'fetching' ? (
-    <LoadingGridView />
-  ) : jobs.data?.length === 0 ? (
+  return jobs.data?.length === 0 ? (
     <Empty />
+  ) : view === 'kanban' ? (
+    <div>kanban</div>
+  ) : view === 'list' ? (
+    <div>list</div>
   ) : (
-    <GridView jobByStatus={jobByStatus} />
+    <>
+      {jobs.status === 'fetching' ? (
+        <LoadingGridView />
+      ) : (
+        <GridView jobByStatus={jobByStatus} />
+      )}
+    </>
   );
-}
+};
 
 export default JobSection;
