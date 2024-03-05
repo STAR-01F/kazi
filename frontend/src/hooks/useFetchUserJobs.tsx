c7360eb1363345880145c2847a6c93ab532587c7
rickyadriell <53133228+rickyadriell@users.noreply.github.com>
2024-03-05 23:41:06 +0000

    chore: [TRAC-143] Add useFetchUserJobs hook

---
+++ frontend/src/hooks/useFetchUserJobs.tsx
@@ -0,0 +1,36 @@
+import {useAuth} from '@services/firebase/hooks/useAuth';
+import {GetJobsByUserID} from '@services/firebase/userJobs';
+import {useState, useEffect} from 'react';
+import {UserJob} from 'src/@types';
+
+const useFetchUserJobs = () => {
+  const [status, setStatus] = useState('idle');
+  const [data, setData] = useState<UserJob[]>();
+  const {user} = useAuth();
+
+  useEffect(() => {
+    if (!user) return;
+    const fetchData = async () => {
+      setStatus('fetching');
+      try {
+        const response = await GetJobsByUserID(user.uid);
+
+        if (response.status === 'Success') {
+          setData(response.data);
+          return;
+        }
+        setStatus('error');
+      } catch (error) {
+        setStatus('error');
+        console.error('Error fetching data:', error);
+      } finally {
+        setStatus('fetched');
+      }
+    };
+    fetchData();
+  }, [user]);
+
+  return {status, data};
+};
+
+export default useFetchUserJobs;
