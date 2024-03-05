c63ebd54118790c9d3a744508e05f75208b4ac74
rickyadriell <53133228+rickyadriell@users.noreply.github.com>
2024-03-05 23:40:32 +0000

    refactor: [TRAC-143] Refactor useFetchJobs hook

--- frontend/src/hooks/useFetchJobs.tsx
+++ frontend/src/hooks/useFetchJobs.tsx
@@ -1,9 +1,9 @@
 import {useAuth} from '@services/firebase/hooks/useAuth';
-import {GetJobByJobID, GetJobsByUserID} from '@services/firebase/jobs';
+import {GetJobByJobID} from '@services/firebase/jobs';
 import {useState, useEffect} from 'react';
-import { Job } from 'src/@types';
+import {Job} from 'src/@types';
 
-const useFetchJobs = (jobid: string = '') => {
+const useFetchJobs = (jobid: string) => {
   const [status, setStatus] = useState('idle');
   const [data, setData] = useState<Job[]>();
   const {user} = useAuth();
@@ -20,14 +20,7 @@ const useFetchJobs = (jobid: string = '') => {
             return;
           }
         }
-        const response = await GetJobsByUserID(user.uid);
-
-        if (response.status === 'Success') {
-          setData(response.data);
-          return;
-        }
         setStatus('error');
-        console.error('Error fetching data:', response.message);
       } catch (error) {
         setStatus('error');
         console.error('Error fetching data:', error);
