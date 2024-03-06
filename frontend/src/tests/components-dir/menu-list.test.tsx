0c447c4322dade665b243dd7126a4f13e441ed73
Tolu <tlawal86@hotmail.co.uk>
2024-03-06 19:45:46 +0000

    test: [TRAC-147] initial tests

---
+++ frontend/src/tests/components-dir/menu-list.test.tsx
@@ -0,0 +1,19 @@
+import MenuListButton from "@components/button/MenuListButton";
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+
+
+describe("MenuListButton", () => {
+  it("should render with the expected inputs", () => {
+    render(
+      <MenuListButton menuActionList={[]} variant="contained">
+        Click me
+      </MenuListButton>
+    );
+
+    const titleElement = screen.getByRole('heading')
+
+
+    expect(titleElement).toBeInTheDocument()
+});
+})
