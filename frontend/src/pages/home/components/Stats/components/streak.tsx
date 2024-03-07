4b1aad91ef0dbd08b26a42d0d080a8b930011ca0
abmutungi <79641169+abmutungi@users.noreply.github.com>
2024-03-07 16:17:25 +0000

    fix: [TRAC-140] Update Stats component and added Streak component

---
+++ frontend/src/pages/home/components/Stats/components/streak.tsx
@@ -0,0 +1,19 @@
+import {Typography} from '@mui/material';
+import {useJobs} from '@services/firebase/hooks/useJobs';
+import isStreak from '@utils/isStreak';
+
+const Streak = () => {
+  const {jobs} = useJobs();
+
+  const streak = isStreak(
+    jobs.map((job) => job.statusUpdates['Saved'].toDate())
+  );
+
+  return (
+    <Typography fontWeight={600} variant="h1" color="textSecondary">
+      {streak?.currentStreak}
+    </Typography>
+  );
+};
+
+export default Streak;
