994ad4777fd95a40b2f08e2ce529ad44aa510156
abmutungi <79641169+abmutungi@users.noreply.github.com>
2024-03-07 14:26:22 +0000

    fix: [TRAC-139] Updated format of stats section

--- frontend/src/pages/home/components/Stats/components/jobsbystatus.tsx
+++ frontend/src/pages/home/components/Stats/components/jobsbystatus.tsx
@@ -13,14 +13,14 @@ const JobsByStatus = () => {
 
   return (
     <PieChart
-      height={250}
+      height={190}
       series={[
         {
           data: [
-            {label: 'Saved', value: getJobCount('Saved')},
-            {label: 'Interview', value: getJobCount('Interview')},
-            {label: 'Applied', value: getJobCount('Applied')},
-            {label: 'Rejected', value: getJobCount('Rejected')},
+            { label: 'Saved', value: getJobCount('Saved') },
+            { label: 'Interview', value: getJobCount('Interview') },
+            { label: 'Applied', value: getJobCount('Applied') },
+            { label: 'Rejected', value: getJobCount('Rejected') },
           ],
           innerRadius: 30,
           outerRadius: 80,
@@ -28,21 +28,14 @@ const JobsByStatus = () => {
           cornerRadius: 5,
           startAngle: 0,
           endAngle: 360,
-          faded: {innerRadius: 30, additionalRadius: -30, color: 'gray'},
-          highlightScope: {faded: 'global', highlighted: 'item'},
+          faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
+          highlightScope: { faded: 'global', highlighted: 'item' },
           cx: '60%',
           cy: '50%',
         },
       ]}
       slotProps={{
-        legend: {
-          position: {vertical: 'bottom', horizontal: 'middle'},
-          direction: 'row',
-          itemMarkWidth: 20,
-          itemMarkHeight: 2,
-          markGap: 5,
-          itemGap: 10,
-        },
+        legend: {hidden: true},
       }}
     />
   );
