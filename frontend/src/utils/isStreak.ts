7fadfaa9d87a3df69dfeef86e30fc5bc5debf540
abmutungi <79641169+abmutungi@users.noreply.github.com>
2024-03-08 15:51:30 +0000

    fix [minor]: [TRAC-140] Fix streak calculation and add console logs for debugging

--- frontend/src/utils/isStreak.ts
+++ frontend/src/utils/isStreak.ts
@@ -1,37 +1,37 @@
 const isStreak = (dates: Date[]) => {
+  console.log(dates, 'dates');
   // Sort the dates in ascending order
   dates.sort((a, b) => new Date(a).getTime() - new Date(b).getTime());
 
-  let longestStreak = 0;
-  let currentStreak = 0;
+  let currentStreak = 1;
+  let longestStreak = 1;
 
-  for (let i = 1; i < dates.length; i++) {
+  for (let i = 1; i < dates.length - 1; i++) {
     const dateBefore = new Date(dates[i - 1]);
     const currentDate = new Date(dates[i]);
 
+    // Set the time of both dates to 00:00:00 for an accurate comparison
     dateBefore.setHours(0, 0, 0, 0);
     currentDate.setHours(0, 0, 0, 0);
 
     // Add one day to dateBefore
     dateBefore.setDate(dateBefore.getDate() + 1);
 
-    // Check if currentDate is not the day after dateBefore
-    if (currentDate.getTime() !== dateBefore.getTime()) {
-    currentStreak ++
-    } else {
-    longestStreak = Math.max(longestStreak, currentStreak);
-    currentStreak = 1;
-    }
-    
-    //
-    longestStreak = Math.max(longestStreak, currentStreak);
+    console.log(dateBefore, currentDate, 'dateBefore, currentDate');
 
-    return {
-      bestStreak: longestStreak,
-      currentStreak: currentStreak,
-      isPB: longestStreak === currentStreak,
-    };
+    // Check if currentDate is the day after dateBefore
+    if (currentDate.getTime() === dateBefore.getTime()) {
+      currentStreak++;
+      longestStreak = Math.max(longestStreak, currentStreak);
+    } else {
+      currentStreak = 1;
+    }
   }
+  return {
+    bestStreak: longestStreak,
+    currentStreak: currentStreak,
+    isPB: longestStreak === currentStreak,
+  };
 };
 
 export default isStreak;
