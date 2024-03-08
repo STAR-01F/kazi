4ab5bf6dfe76f7ed75d2987329e9271f9d6d4a4a
abmutungi <79641169+abmutungi@users.noreply.github.com>
2024-03-08 23:29:23 +0000

    fix: [TRAC-140] Refactor streak calculation in Stats component

--- frontend/src/utils/isStreak.ts
+++ frontend/src/utils/isStreak.ts
@@ -1,36 +1,63 @@
 const isStreak = (dates: Date[]) => {
   console.log(dates, 'dates');
-  // Sort the dates in ascending order
-  dates.sort((a, b) => new Date(a).getTime() - new Date(b).getTime());
+  if (dates.length === 0) {
+    return {bestStreak: 0, currentStreak: 0, isPB: true};
+  }
 
-  let currentStreak = 1;
+  // Convert and sort dates, ensuring time components are ignored
+  const uniqueSortedDates = Array.from(
+    new Set(dates.map((date) => new Date(date).setHours(0, 0, 0, 0)))
+  )
+    .sort((a, b) => a - b)
+    .map((date) => new Date(date)); // Convert timestamps back to Date objects
+
+  if (uniqueSortedDates.length === 0) {
+    return {bestStreak: 0, currentStreak: 0, isPB: true};
+  }
+
   let longestStreak = 1;
+  let currentStreak = 1;
+  let previousDate = uniqueSortedDates[0];
 
-  for (let i = 1; i < dates.length - 1; i++) {
-    const dateBefore = new Date(dates[i - 1]);
-    const currentDate = new Date(dates[i]);
+  for (let i = 1; i < uniqueSortedDates.length; i++) {
+    const currentDate = uniqueSortedDates[i];
+    const differenceInDays =
+      (currentDate.getTime() - previousDate.getTime()) / (1000 * 3600 * 24);
 
-    // Set the time of both dates to 00:00:00 for an accurate comparison
-    dateBefore.setHours(0, 0, 0, 0);
-    currentDate.setHours(0, 0, 0, 0);
-
-    // Add one day to dateBefore
-    dateBefore.setDate(dateBefore.getDate() + 1);
-
-    console.log(dateBefore, currentDate, 'dateBefore, currentDate');
-
-    // Check if currentDate is the day after dateBefore
-    if (currentDate.getTime() === dateBefore.getTime()) {
+    if (differenceInDays === 1) {
       currentStreak++;
-      longestStreak = Math.max(longestStreak, currentStreak);
     } else {
+      // Reset current streak if dates are not consecutive
       currentStreak = 1;
     }
+
+    if (currentStreak > longestStreak) {
+      longestStreak = currentStreak;
+    }
+
+    previousDate = currentDate;
   }
+
+  // Determine current streak based on the most recent date's proximity to today's date
+  const today = new Date(new Date().setHours(0, 0, 0, 0));
+  const mostRecentDate = uniqueSortedDates[uniqueSortedDates.length - 1];
+  if (!mostRecentDate) {
+    return {
+      bestStreak: longestStreak,
+      currentStreak: 0,
+      isPB: longestStreak === 0,
+    }; // Safe guard against undefined mostRecentDate
+  }
+  const differenceToToday =
+    (today.getTime() - mostRecentDate.getTime()) / (1000 * 3600 * 24);
+
+  // If the most recent date is not today, reset current streak
+  const finalCurrentStreak = differenceToToday === 0 ? currentStreak : 0;
+
   return {
     bestStreak: longestStreak,
-    currentStreak: currentStreak,
-    isPB: longestStreak === currentStreak,
+    currentStreak: finalCurrentStreak,
+    isPB: longestStreak === finalCurrentStreak,
   };
 };
 
