051ef4270baac22e6a578bd8f6b06e202a94da5e
eternal17 <sarmadkhatri15@gmail.com>
2024-03-11 16:17:02 +0000

    refactor: [TRAC-154] wrapped the notes into a card

--- frontend/src/pages/job/components/Notes/Notes.tsx
+++ frontend/src/pages/job/components/Notes/Notes.tsx
@@ -1,5 +1,12 @@
 import {useState, useEffect} from 'react';
-import {Grid, TextField, Button} from '@mui/material';
+import {
+  Grid,
+  TextField,
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+} from '@mui/material';
 import {UpdateUserJobNotes} from '@services/firebase/userJobs';
 import {useFeedback} from '@hooks/useFeeback';
 import {useAuth} from '@services/firebase/hooks/useAuth';
@@ -39,19 +46,44 @@ const Notes = ({userJob}: NotesProps) => {
   };
 
   return (
-    <Grid alignItems={'center'} justifyContent={'center'} container item>
-      <TextField
-        id="outlined-multiline-static"
-        label="notes"
-        multiline
-        rows={15}
-        fullWidth={true}
-        value={notesData}
-        onChange={(e) => setNotesData(e.target.value)}
-      />
+    <Grid
+      justifyContent={'center'}
+      flexDirection={'column'}
+      container
+      item
+
+    >
+      <Card sx={{width: '100%'}}>
+        <CardHeader
+          style={{
+            borderRadius: '5px 5px 0 0',
+            background: '#D5D5D5',
+            color: 'black',
+          }}
+          title="Notes"
+          titleTypographyProps={{fontSize: '1.2rem', fontWeight: 'bold'}}
+          sx={{height: '2.5rem'}}
+        />
+        <CardContent sx={{width: '100%'}}>
+          <TextField
+            sx={{
+              minHeight: '300px',
+            }}
+            id="outlined-multiline-static"
+            variant="standard"
+            multiline
+            rows={15}
+            fullWidth={true}
+            value={notesData}
+            onChange={(e) => setNotesData(e.target.value)}
+          />
+        </CardContent>
+      </Card>
+
       <Button
         onClick={() => handleUpdateJobNotes(notesData)}
         variant="contained"
+        sx={{mt: '1rem', alignSelf: 'end'}}
       >
         Save
       </Button>
