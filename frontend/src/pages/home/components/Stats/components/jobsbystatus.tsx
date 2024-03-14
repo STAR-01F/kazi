7de832c640eae201108f80637f27d23e25ccda47
rickyadriell <53133228+rickyadriell@users.noreply.github.com>
2024-03-14 00:30:56 +0000

    chore: [TRAC-167] Update breakdown chart in stats components

--- frontend/src/pages/home/components/Stats/components/jobsbystatus.tsx
+++ frontend/src/pages/home/components/Stats/components/jobsbystatus.tsx
@@ -1,25 +1,47 @@
 import {useJobs} from '@services/firebase/hooks/useJobs';
 import {JobByStatus, JobStatus, groupJobsByStatus} from '@utils/groupJobStatus';
 import {PieChart} from '@mui/x-charts/PieChart';
+import {useEffect, useState} from 'react';
+import {PieValueType} from '@mui/x-charts';
+import {MakeOptional} from '@mui/x-charts/models/helpers';
+import jobStatus from '@repository/job.json';
+import {TriggerOptions} from '@mui/x-charts/ChartsTooltip/utils';
 
 const JobsByStatus = () => {
   const {jobs} = useJobs();
-
-  const alljobs: JobByStatus = groupJobsByStatus(jobs);
-  const getJobCount = (status: JobStatus) =>
-    alljobs && alljobs[status] ? alljobs[status].length : 0;
-
+  const [data, setData] = useState([{}]);
+  const [trigger, setTrigger] = useState<TriggerOptions | undefined>('item');
+  useEffect(() => {
+    const alljobs: JobByStatus = groupJobsByStatus(jobs);
+    const jobStatusStats = jobStatus.status.reduce(
+      (acc, status) => {
+        const value =
+          alljobs && alljobs[status as JobStatus]
+            ? alljobs[status as JobStatus].length
+            : 0;
+        if (value && value > 0) {
+          acc.push({
+            label: status,
+            value: value || 0,
+          });
+        }
+        return acc;
+      },
+      [] as MakeOptional<PieValueType, 'id'>[]
+    );
+    if (jobStatusStats.length === 0) {
+      jobStatusStats.push({value: 100, color: 'rgba(0, 0, 0, 0.1)'});
+      setTrigger('none');
+    }
+    setData(jobStatusStats);
+  }, [jobs]);
   return (
     <PieChart
       height={190}
+      tooltip={{trigger}}
       series={[
         {
-          data: [
-            {label: 'Saved', value: getJobCount('Saved')},
-            {label: 'Interview', value: getJobCount('Interview')},
-            {label: 'Applied', value: getJobCount('Applied')},
-            {label: 'Rejected', value: getJobCount('Rejected')},
-          ],
+          data: data as MakeOptional<PieValueType, 'id'>[],
           innerRadius: 30,
           outerRadius: 80,
           paddingAngle: 5,
@@ -28,7 +50,7 @@ const JobsByStatus = () => {
           endAngle: 360,
           faded: {innerRadius: 30, additionalRadius: -30, color: 'gray'},
           highlightScope: {faded: 'global', highlighted: 'item'},
-          cx: '70%',
+          cx: '137%',
           cy: '50%',
         },
       ]}
