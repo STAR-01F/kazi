d0a4353d812e81cbcbeef385a78b63fae1e8c08f
rickyadriell <53133228+rickyadriell@users.noreply.github.com>
2024-03-19 18:22:32 +0000

    style: [TRAC-131] Add Google Fonts and customize theme

--- landing/src/themes/theme.tsx
+++ landing/src/themes/theme.tsx
@@ -1,14 +1,14 @@
-import { ThemeProvider } from '@emotion/react';
-import { CssBaseline, createTheme } from '@mui/material';
-import { useMemo } from 'react';
-import { breakpoints } from './customs';
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline, createTheme } from "@mui/material";
+import { useMemo } from "react";
+import { breakpoints } from "./customs";
 
 /**
  * ThemeOverideProps
  *
  */
 type ThemeCustomizationProps = {
-    children: React.ReactNode;
+  children: React.ReactNode;
 };
 
 /**
@@ -29,42 +29,91 @@ type ThemeCustomizationProps = {
  * ```
  */
 const ThemeCustomization = ({ children }: ThemeCustomizationProps) => {
-    const theme = useMemo(
-        () =>
-            createTheme({
-                breakpoints: { ...breakpoints },
-                palette: {
-                    primary: {
-                        main: '#8db3e1',
-                    },
-                },
-                components: {
-                    MuiCssBaseline: {
-                        styleOverrides: (theme) => ({
-                            '::-webkit-scrollbar': {
-                                width: '10px',
-                                backgroundColor: theme.palette.background.paper,
-                            },
-                            '::-webkit-scrollbar-track': {
-                                boxShadow: `inset 0 0 6px ${theme.palette.divider}`,
-                            },
-                            '::-webkit-scrollbar-thumb': {
-                                backgroundColor: theme.palette.divider,
-                                outline: `1px solid ${theme.palette.divider}`,
-                                borderRadius: '8px',
-                            },
-                        }),
-                    },
-                },
+  // const prefersDarkMode = useMediaQuery('(prefers-color-scheme: light)');
+  const baseTheme = createTheme();
+  const theme = useMemo(
+    () =>
+      createTheme({
+        breakpoints: { ...breakpoints },
+        palette: {
+          //mode: prefersDarkMode ? 'dark' : 'light',
+          primary: baseTheme.palette.augmentColor({
+            color: {
+              main: "#000",
+            },
+          }),
+          background: {
+            paper: "#f0f0f6",
+            default: "#f0f0f3",
+          },
+        },
+        typography: {
+          fontFamily: [
+            // 'Kanit',
+            // 'Ysabeau Office',
+            // 'Young Serif',
+            "Oswald",
+            // 'Arial',
+            '"Helvetica Neue"',
+            "-apple-system",
+            "sans-serif",
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+          ].join(","),
+        },
+        components: {
+          MuiCssBaseline: {
+            styleOverrides: (theme) => ({
+              "::-webkit-scrollbar": {
+                width: "10px",
+                backgroundColor: theme.palette.background.paper,
+              },
+              "::-webkit-scrollbar-track": {
+                boxShadow: `inset 0 0 6px ${theme.palette.divider}`,
+              },
+              "::-webkit-scrollbar-thumb": {
+                backgroundColor: theme.palette.divider,
+                outline: `1px solid ${theme.palette.divider}`,
+                borderRadius: "8px",
+              },
             }),
-        []
-    );
-    return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            {children}
-        </ThemeProvider>
-    );
+          },
+          MuiPaper: {
+            styleOverrides: {
+              root: {
+                border: "1px solid " + baseTheme.palette.divider,
+                boxShadow: "none",
+                borderRadius: "16px",
+              },
+            },
+          },
+          MuiButton: {
+            styleOverrides: {
+              root: {
+                textTransform: "none",
+                borderRadius: "16px",
+                justifyContent: "center",
+                alignItems: "center",
+                textAlign: "center",
+                px: "10px",
+                "&.MuiButton-contained": {
+                  boxShadow: "none",
+                },
+              },
+            },
+          },
+        },
+      }),
+    // [prefersDarkMode]
+    [baseTheme],
+  );
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  );
 };
 
 export default ThemeCustomization;
