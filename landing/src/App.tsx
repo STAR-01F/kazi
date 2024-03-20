126b71b0664db9b8be5567f49f1355803b160e32
rickyadriell <53133228+rickyadriell@users.noreply.github.com>
2024-03-20 15:12:28 +0000

    style: [TRAC-131] Add landing page sections

--- landing/src/App.tsx
+++ landing/src/App.tsx
@@ -1,99 +1,43 @@
-import { Box, Button, Container, Grid, Typography } from '@mui/material';
-import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+import SectionTwo from "./components/section/SectionTwo";
+import SectionThree from "./components/section/SectionThree";
+import RenderPageImage from "./components/PageImage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import SectionOne from "./components/section/SectionOne";
 
 function App() {
-    const siteUrl = import.meta.env.VITE_BETA_SITE_URL as string;
-    return (
-        <Box
-            component={Grid}
-            container
-            width={'100vw'}
-            height={'100vh'}
-            sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                background:
-                    'radial-gradient(circle, hsla(163, 100%, 93%, 1) 0%, hsla(190, 58%, 85%, 1) 0%, hsla(167, 86%, 92%, 1) 0%, hsla(213, 59%, 72%, 1) 100%);',
-            }}>
-            <Container
+  return (
+    <>
+      <RenderPageImage />
+      <Header />
+      <SectionOne />
+      <SectionTwo />
+      <SectionThree />
+      {/* <Box
                 component={Grid}
                 container
                 width={'100%'}
-                maxWidth={'xs'}
-                height={'100vh'}
-                direction={'column'}
+                height={'500px'}
                 sx={{
                     display: 'flex',
                     justifyContent: 'center',
                     alignItems: 'center',
-                }}>
-                <Box
-                    component={'img'}
-                    src='/kazi.png'
-                    height={'100px'}
-                    mb={5}
-                />
-                <Container
-                    component={Grid}
-                    container
-                    width={'100%'}
-                    mb={5}
-                    sx={{
-                        display: 'flex',
-                        justifyContent: 'space-around',
-                        alignItems: 'center',
-                    }}>
-                    <Typography variant='h6' fontWeight={'bold'}>
-                        Simplify
-                    </Typography>
-                    <Typography variant='h6' fontWeight={'bold'}>
-                        ּ•
-                    </Typography>
-                    <Typography variant='h6' fontWeight={'bold'}>
-                        Track
-                    </Typography>
-                    <Typography variant='h6' fontWeight={'bold'}>
-                        ּ•
-                    </Typography>
-                    <Typography variant='h6' fontWeight={'bold'}>
-                        Succeed
-                    </Typography>
-                </Container>
-                <Container
-                    component={Grid}
-                    container
-                    width={'100%'}
-                    mb={10}
-                    sx={{
-                        display: 'flex',
-                        justifyContent: 'space-around',
-                        alignItems: 'center',
-                    }}>
-                    <Typography textAlign={'justify'}>
-                        Unlock your career's full potential with Kazi.
-                    </Typography>
-                </Container>
-                <Container
-                    component={Grid}
-                    container
-                    width={'100%'}
-                    sx={{
-                        display: 'flex',
-                        justifyContent: 'space-around',
-                        alignItems: 'center',
-                    }}>
-                    <Button
-                        startIcon={<OpenInNewIcon />}
-                        LinkComponent={'a'}
-                        href={siteUrl}
-                        variant='contained'>
-                        Explore Beta
-                    </Button>
-                </Container>
-            </Container>
-        </Box>
-    );
+                    backgroundColor: '#F8F8F8',
+                }}></Box>
+            <Box
+                component={Grid}
+                container
+                width={'100%'}
+                height={'500px'}
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    backgroundColor: '#F3F3F3',
+                }}></Box> */}
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ landing/src/components/section/SectionOne.tsx
@@ -0,0 +1,90 @@
+import { Box, Button, Container, Grid, Typography } from "@mui/material";
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+
+const SectionOne = () => {
+  const siteUrl = import.meta.env.VITE_BETA_SITE_URL as string;
+  return (
+    <Box
+      component={Grid}
+      container
+      width={"100%"}
+      height={"650px"}
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <Container
+        component={Grid}
+        container
+        width={"100%"}
+        maxWidth={"lg"}
+        height={"400px"}
+        direction={"column"}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box component={"img"} src="/kazi.png" height={"70px"} mb={1} />
+
+        <Container
+          component={Grid}
+          container
+          width={"100%"}
+          sx={{
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
+          }}
+        >
+          <Typography
+            textAlign={"justify"}
+            fontWeight={"bold"}
+            fontSize={{ xs: "1.5rem", md: "3rem" }}
+          >
+            Unlock your career's full potential
+          </Typography>
+        </Container>
+        <Container
+          component={Grid}
+          container
+          mb={3}
+          sx={{
+            width: "350px",
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
+          }}
+        >
+          <Typography fontSize={"1.2rem"}>
+            Simplify ּ• Track • Succeed
+          </Typography>
+        </Container>
+        <Container
+          component={Grid}
+          container
+          width={"100%"}
+          sx={{
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
+          }}
+        >
+          <Button
+            startIcon={<OpenInNewIcon />}
+            LinkComponent={"a"}
+            href={siteUrl}
+            size="large"
+            variant="contained"
+          >
+            Explore
+          </Button>
+        </Container>
+      </Container>
+    </Box>
+  );
+};
+
+export default SectionOne;
