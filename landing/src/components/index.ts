3cd4d5f23ebb45b64d35e9a0e0af8306dceabee2
rickyadriell <53133228+rickyadriell@users.noreply.github.com>
2024-03-20 17:12:40 +0000

    build: [TRAC-131] Refactor import statements in App.tsx

---
+++ landing/src/components/index.ts
@@ -0,0 +1,15 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import RenderPageImage from "./PageImage";
+import SectionOne from "./section/SectionOne";
+import SectionThree from "./section/SectionThree";
+import SectionTwo from "./section/SectionTwo";
+
+export {
+  Header,
+  Footer,
+  RenderPageImage,
+  SectionOne,
+  SectionThree,
+  SectionTwo,
+};
